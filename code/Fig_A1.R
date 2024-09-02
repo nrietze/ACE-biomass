a82956d826f5f705893c55590bf00e3128c45af8
nrietze <nils.rietze@uzh.ch>
2024-09-02 18:49:05 +0200

    added a new subplot for Fig A1.

--- code/Fig_A1.R
+++ code/Fig_A1.R
@@ -5,6 +5,7 @@
 library(tidyverse)
 library(tidyterra)
 library(terra)
+library(sf)
 library(grid)
 library(cowplot)
 library(RColorBrewer)
@@ -66,56 +67,179 @@ if (shifted_extract){
 }
 # 3. Plot figure ----
 
-min_x <- 533832.9
-min_y <- 8837356
+## a) artificial 10 m plots ----
+min_x <- 533831.9
+min_y <- 8837358
 
 # Data frame for scale bar
 rect_data_h <- data.frame(
   xmin = min_x,
   xmax = min_x + 10,
-  ymin = min_y, 
-  ymax = min_y + .8,
-  ytext = min_y + .4,
+  ymin = min_y - 1, 
+  ymax = min_y - 1.8,
+  ytext = min_y - 1.4,
   xtext = min_x +5
 )
 
 rect_data_v <- data.frame(
-  xmin = min_x + 13,
-  xmax = min_x + 13.8,
+  xmin = min_x + 14,
+  xmax = min_x + 14.8,
   ymin = min_y + 3, 
   ymax = min_y + 3 + 10,
   ytext = min_y + 8,
-  xtext = min_x + 13.4
+  xtext = min_x + 14.4
 )
 
-p <- ggplot() +
+pos_cent <- 25
+pos1 <- 20
+pos2 <- 37
+
+p1 <- ggplot() +
+  # draw square boxes
   geom_spatvector(data = buffered_points,color = "gray", fill = NA) +
-  geom_spatvector(data = buffered_points[20,],color = "brown3", fill = NA, 
+  geom_spatvector(data = buffered_points[pos_cent,],color = "black", fill = NA, 
                   linewidth = 2) + 
-  geom_spatvector(data = buffered_points[30,],color = "#00008B", fill = NA, 
-                  linewidth = 2) + 
+  geom_spatvector(data = buffered_points[pos1,],color = "brown3", fill = NA, 
+                  linewidth = 1) + 
+  geom_spatvector(data = buffered_points[pos2,],color = "#00008B", fill = NA, 
+                  linewidth = 1) + 
   geom_spatvector(data = plot_locations_shifted, color = "gray") +
+  # draw points
   geom_spatvector(data = plot_locations, color = "black", 
                   size = 2) +
   geom_spatvector(data = plot_locations, color = "black", 
                   shape = 4, size = 5) +
-  geom_spatvector(data = plot_locations_shifted[20,], color = "brown3",
+  geom_spatvector(data = plot_locations_shifted[pos1,], color = "brown3",
                   size = 2) +
-  geom_spatvector(data = plot_locations_shifted[30,], color = "#00008B", 
+  geom_spatvector(data = plot_locations_shifted[pos2,], color = "#00008B", 
                   size = 2) +
   # horizontal scale bar
   geom_rect(data = rect_data_h, aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), 
-            fill = '#00008B') +
+            fill = 'black') +
   geom_text(data = rect_data_h, aes(x = xtext, y = ytext),
             label = "10 m", fontface = "bold", color = 'white') +
   # vertical scale bar
   geom_rect(data = rect_data_v, aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), 
-            fill = '#00008B') +
+            fill = 'black') +
   geom_text(data = rect_data_v, aes(x = xtext, y = ytext),
             label = "10 m", color = 'white', fontface = "bold",
             angle = 90) +
-  theme_map()
+  theme_map();p1
+
+## b) Subplot figure ----
+
+# Function to create random squares
+create_random_squares <- function(num_squares, side_length, area_extent) {
+  squares <- list()
+  
+  for (i in 1:num_squares) {
+    # Random center within the area
+    x_center <- runif(1, min = area_extent$xmin, max = area_extent$xmax)
+    y_center <- runif(1, min = area_extent$ymin, max = area_extent$ymax)
+    
+    # Random angle
+    angle <- runif(1, min = 0, max = 2 * pi)
+    
+    # Define vertices of the square
+    half_side <- side_length / 2
+    corners <- matrix(c(
+      -half_side, -half_side,
+      half_side, -half_side,
+      half_side,  half_side,
+      -half_side,  half_side
+    ), ncol = 2, byrow = TRUE)
+    
+    # Rotate the corners
+    rotation_matrix <- matrix(c(
+      cos(angle), -sin(angle),
+      sin(angle),  cos(angle)
+    ), ncol = 2, byrow = TRUE)
+    
+    rotated_corners <- t(rotation_matrix %*% t(corners))
+    
+    # Translate the corners to the center
+    rotated_corners <- rotated_corners + c(x_center, y_center)
+    
+    x_values <- c(rotated_corners[1, ], rotated_corners[3, ])
+    y_values <- c(rotated_corners[2, ], rotated_corners[4, ])
+    
+    # Create a polygon (SpatVector)
+    df <- data.frame(id = i,
+               x = x_values,
+               y = y_values) 
+    df <- rbind(df, df[1, ])
+    
+    sf_polygon <- st_as_sf(
+      st_sfc(st_polygon(list(as.matrix(df[, c("x", "y")])))),
+      crs = 32643  # Specify the CRS if needed
+    )
+    
+    polygon <- vect(sf_polygon)
+    
+    squares[[i]] <- polygon
+  }
+  
+  # Combine into a single SpatVector
+  combined_squares <- do.call(rbind, squares)
+  return(combined_squares)
+}
+
+# Define the extent of the area to place squares within
+area_extent <- ext(buffered_points[pos_cent,])
+
+# Create 3 random squares of side length 25 cm
+set.seed(1)
+random_squares <- create_random_squares(3, 0.25, area_extent)
+
+# Data frame for scale bar
+rect_data_h <- data.frame(
+  xmin = min_x,
+  xmax = min_x + 10,
+  ymin = min_y + 1, 
+  ymax = min_y + 1.8,
+  ytext = min_y + 1.4,
+  xtext = min_x +5
+)
+
+rect_data_v <- data.frame(
+  xmin = min_x + 11,
+  xmax = min_x + 11.8,
+  ymin = min_y + 3, 
+  ymax = min_y + 3 + 10,
+  ytext = min_y + 8,
+  xtext = min_x + 11.4
+)
+
+p2 <- ggplot() +
+  # draw square boxes
+  geom_spatvector(data = buffered_points[pos_cent,],color = "black", fill = NA, 
+                  linewidth = 2) +  
+  geom_spatvector(data = random_squares,color = "blue", fill = NA, 
+                  linewidth = 1) +
+  # draw points
+  geom_spatvector(data = plot_locations, color = "black", 
+                  size = 2) +
+  geom_spatvector(data = plot_locations, color = "black", 
+                  shape = 4, size = 5) +
+  # horizontal scale bar
+  geom_rect(data = rect_data_h, aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), 
+          fill = 'black') +
+  geom_text(data = rect_data_h, aes(x = xtext, y = ytext),
+            label = "10 m", fontface = "bold", color = 'white') +
+  # vertical scale bar
+  geom_rect(data = rect_data_v, aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), 
+            fill = 'black') +
+  geom_text(data = rect_data_v, aes(x = xtext, y = ytext),
+            label = "10 m", color = 'white', fontface = "bold",
+            angle = 90) +
+  theme_map();p2
+
+
+## c) Create final figure ----
+pg <- cowplot::plot_grid(p1,p2, nrow = 2,
+                         labels = c("a)","b)") # add subplot labels
+                         )
 
 # export PNG
-ggsave(p,filename = paste0(FIG_PATH,"Fig_A1.png"),
-        bg = 'white',width = 8, height = 8)
+ggsave(pg,filename = paste0(FIG_PATH,"Fig_A1.png"),
+       bg = 'white',width = 6, height = 12)
